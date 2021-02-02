fec388723db05a2064a2f4f9db9d3a688ae1e48f
Muneiah <muneiahtellakula@gmail.com>
2021-02-02 10:46:03 +0530

    Second day(2-2-2021)

---
+++ UiControls/app/src/main/java/com/example/uicontrols/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.uicontrols;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.RatingBar;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    //ActivityMainBinding binding;
+    RatingBar rating;
+    SeekBar seekBar;
+    TextView tv_rating,tv_seekbar;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        rating=findViewById(R.id.rb);
+        seekBar=findViewById(R.id.sb);
+        tv_rating=findViewById(R.id.tv_rating);
+        tv_seekbar=findViewById(R.id.tv_progress);
+        rating.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+            @Override
+            public void onRatingChanged(RatingBar ratingBar, float rate, boolean fromUser) {
+                tv_rating.setText("User Rating is: "+ rate);
+            }
+        });
+
+    }
+}
