4efd46d241e3e2a9a7b45a9b8cccd5da17bf910e
Muneiah <muneiahtellakula@gmail.com>
2021-02-02 12:39:56 +0530

    Second day(2-2-2021)

---
+++ LoginRegPage/app/src/main/java/com/example/loginregpage/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.loginregpage;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.loginregpage.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+ActivityMainBinding binding;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding= DataBindingUtil.setContentView(this,R.layout.activity_main);
+    }
+
+    public void loginEvent(View view) {
+    }
+
+    public void registerEvent(View view) {
+        /*Intent :An Intetion to perfom navigating for one activity/screen to another activty/screen*/
+   /*two types
+   * 1.Explicit Intent :startActivity(new Intent(this,desti.clss));
+   * 2.Implicit Intent */
+        startActivity(new Intent(this,RegisterActivity.class));
+    }
+}
