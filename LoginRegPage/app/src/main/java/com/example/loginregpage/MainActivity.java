063a5152c6214c7fff7aa806177a6d076ef7f9b1
Muneiah <muneiahtellakula@gmail.com>
2021-02-02 16:38:28 +0530

    Second day(2-2-2021)

--- LoginRegPage/app/src/main/java/com/example/loginregpage/MainActivity.java
+++ LoginRegPage/app/src/main/java/com/example/loginregpage/MainActivity.java
@@ -4,21 +4,37 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import com.example.loginregpage.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 ActivityMainBinding binding;
-
+DbHelper helper;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding= DataBindingUtil.setContentView(this,R.layout.activity_main);
+    helper=new DbHelper(this);
     }
 
     public void loginEvent(View view) {
+        Cursor c=helper.retriveData();
+        StringBuilder record1=new StringBuilder();
+        StringBuilder record2=new StringBuilder();
+        while (c.moveToNext()){
+            record1.append(c.getString(1));
+            record2.append(c.getString(2));
+
+        }
+        if (binding.etUsername.getText().toString().equals(record1)||binding.etPassword.getText().toString().equals("sai")){
+            Toast.makeText(this, "welcome ", Toast.LENGTH_SHORT).show();
+        }else {
+            Toast.makeText(this, "Faild", Toast.LENGTH_SHORT).show();
+        }
 
     }
 
